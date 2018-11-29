9db7907ed2694d8944e1408135055b2b426a0ecd
aakashDeveloper <ahanda205@gmail.com>
2018-11-29 03:42:53 +0000

    a

---
+++ React/firstapp/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Header extends React.Component {
+    render(){
+        return(
+            <header>
+                <div className="logo">Logo</div>
+                <center>
+                    <input type="text" />
+                </center>  
+            </header>
+        )
+    }
+}
+
+export default Header;
--- React/firstapp/src/index.js
+++ React/firstapp/src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Header from './components/Header';
+import NewsList from './components/News_list';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return (
+        <div>
+            <Header/>
+           <NewsList></NewsList>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
+
+
+
+
+
+
+
+
+
+/*
+
+functional (stateless)
+light in weight and less functionality
+
+function add(a,b){
+    return a+b
+}
+
+let add = (a,b) => {
+    return a+b
+}
+Class based(stateful)
+bit heavy as compare to functional but more functionality
+
+state & props
+
+JSX
+*/
