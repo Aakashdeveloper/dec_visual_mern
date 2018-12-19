a59d36ba5ecf7c802b802993ededec1c68cb5994
aakashDeveloper <ahanda205@gmail.com>
2018-12-19 07:44:42 +0530

    a

--- React/redux2/src/actions/index.js
+++ React/redux2/src/actions/index.js
@@ -1,1 +1,22 @@
 import { baseUrl } from '../constants/ApiUrl';
+
+export function latestNews(){
+    const request = fetch(`${baseUrl}articles?_end=3`,
+    {method:'GET'})
+        .then(response => response.json())
+        return{
+            type:'GET_LATEST',
+            payload: request
+        }
+}
+
+
+export function otherNews(){
+    const request = fetch(`${baseUrl}articles?_start=3&_end=10`,
+    {method:'GET'})
+        .then(response => response.json())
+        return{
+            type:'GET_OTHER',
+            payload: request
+        }
+}
--- React/redux2/src/containers/Home.js
+++ React/redux2/src/containers/Home.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { latestNews } from '../actions';
+import { latestNews, otherNews } from '../actions';
 import { bindActionCreators } from 'redux';
 
 import LatestNews from '../components/home/Latest';
+import OtherNews from '../components/home/Other'
 
 class Home extends Component {
+
+    componentDidMount(){
+        this.props.latestNews()
+        this.props.otherNews();
+    }
+
     render(){
         return(
             <div>
-                <LatestNews/>
+                <LatestNews latest={this.props.articles.latest}/>
+                <OtherNews otherNews={this.props.articles.other}/>
             </div>
         )
     }
@@ -22,7 +30,7 @@ function mapStateToProps(state){
 }
 
 function mapDispatchProps(disptach){
-    return bindActionCreators({latestNews}, disptach)
+    return bindActionCreators({latestNews, otherNews}, disptach)
 }
 
 export default connect(mapStateToProps, mapDispatchProps) (Home);
